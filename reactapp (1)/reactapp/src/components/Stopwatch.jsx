5ddeeea01e6b90de5289ea852205c9aef2bec97f
Iamneo <techadmin@iamneo.ai>
2023-08-09 09:41:13 +0000

    Backup Commit

--- reactapp (1)/reactapp/src/components/Stopwatch.jsx
+++ reactapp (1)/reactapp/src/components/Stopwatch.jsx
@@ -18,11 +18,9 @@ const Stopwatch = () => {
   useEffect(() => {
     let interval = null;
     if (isRunning) {
-      interval = setInterval(() => {
-        setTime((time) => {
-            return time + 1;
-        });
-      }, 1000);
+        interval = setInterval(function () {
+            setTime((time) => time + 1);
+        }, 1000);
     } else if (!isRunning && time !== 0) {
       clearInterval(interval);
     }
